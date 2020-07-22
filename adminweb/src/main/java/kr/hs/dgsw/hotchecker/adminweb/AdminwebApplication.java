1f25611a704955dd7e7572538e3d5553c5acb040
seoyeon <elvina0902@naver.com>
2020-07-22 14:03:21 +0900

    Init

---
+++ adminweb/src/main/java/kr/hs/dgsw/hotchecker/adminweb/AdminwebApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.hotchecker.adminweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminwebApplication.class, args);
+    }
+
+}
---
+++ adminweb/src/test/java/kr/hs/dgsw/hotchecker/adminweb/AdminwebApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.hotchecker.adminweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
